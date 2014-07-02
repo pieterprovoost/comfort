044036b8e480baa5edf194fcc86ee3d04bfbe500
Pieter Provoost <pieterprovoost@gmail.com>
2014-07-03 01:38:26 +0200

    added draught

--- browser.js
+++ browser.js
@@ -112,10 +112,28 @@
 	"comfort": {
 		"comfort.js": function (exports, module, require) {
 			var pmv = require('./lib/pmv');
+			var local = require('./lib/local');
 			
 			module.exports.Pmv = pmv.Pmv;
+			module.exports.Local = local.Local;
 		},
 		"lib": {
+			"local.js": function (exports, module, require) {
+				Local = function() {};
+
+				Local.draught = function(ta, va, tu) {
+					if (va < 0.05) {
+						va = 0.05;
+					}
+					var dr = (34 - ta) * Math.pow(va - 0.05, 0.62) * (0.37 * va * tu + 3.14);
+					if (dr > 100) {
+						dr = 100;
+					}
+					return dr;
+				};
+
+				module.exports.Local = Local;
+			},
 			"pmv.js": function (exports, module, require) {
 				var jerzy = require('jerzy');
 
