7158882d7dddb408b0acdda66caa3554896b04d3
Pieter Provoost <pieterprovoost@gmail.com>
2014-07-31 23:08:56 +0200

    whitespace

--- info/dev.js
+++ info/dev.js
@@ -1,7 +1,3 @@
 var comfort = require("../comfort");
 
 console.log(comfort.Local.vertical(10));
-console.log(comfort.Local.warmCeiling(10));
-console.log(comfort.Local.coolWall(10));
-console.log(comfort.Local.coolCeiling(15));
-console.log(comfort.Local.warmWall(20));
